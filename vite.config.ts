58e0e601cdd0cd238fc75f1a9d126ee435bbfdea
michalwdowik <mwdowikm@gmail.com>
2023-05-31 19:19:53 +0200

    Fulfill PWA conditions

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { compression } from 'vite-plugin-compression2'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     server: {
@@ -11,6 +12,35 @@ export default defineConfig({
     },
     plugins: [
         react(),
+        VitePWA({
+            strategies: 'generateSW',
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['**/*.{js,css}'],
+                navigateFallback: null,
+            },
+            devOptions: {
+                enabled: true,
+                type: 'module',
+                navigateFallback: 'index.html',
+            },
+
+            manifest: {
+                id: '/',
+                start_url: './',
+                name: 'Michał Wdowik Dev',
+                short_name: 'Michał Wdowik Dev',
+                theme_color: '#000000',
+                icons: [
+                    {
+                        src: '/favicon.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                ],
+            },
+        }),
         compression({
             include: [/\.(js|mjs|json|css|html)$/],
             exclude: [/\.(br)$/, /\.(gz)$/],
