2581e46e39d2fec0dad8c2647b52dd4c01d07751
michalwdowik <mwdowikm@gmail.com>
2023-07-06 23:52:50 +0200

    Update CV file

--- src/helpers/downloadCV.ts
+++ src/helpers/downloadCV.ts
@@ -1,10 +1,10 @@
 const downloadCV = () => {
-    fetch('michalwdowikCV.pdf').then((response) => {
+    fetch('michalwdowik-cv.pdf').then((response) => {
         response.blob().then((blob) => {
             const fileURL = window.URL.createObjectURL(blob)
             const alink = document.createElement('a')
             alink.href = fileURL
-            alink.download = 'michalwdowikCV.pdf'
+            alink.download = 'michalwdowik-cv.pdf'
             alink.click()
         })
     })
